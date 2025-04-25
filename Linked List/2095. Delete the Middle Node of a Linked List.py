62ce1e9645ec08960611ea92e11ca22386024576
omad <omadbek95@gmail.com>
2025-04-25 12:16:36 +0200

    linked list problems

---
+++ Linked List/2095. Delete the Middle Node of a Linked List.py
@@ -0,0 +1,48 @@
+#https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/?envType=study-plan-v2&envId=leetcode-75
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        """
+        Two pointers approach:
+        Using slow and fast pointers
+
+        We can create two pointers fast pointer will always be n + 2 times ahead of slow pointer,
+        thus when fast pointer will get to the ending we will stop pointer and slow pointer will be
+        in position where it will be near the target that should be removed.
+
+        Slow pointer: n
+        Fast pointer: n+2
+
+        By removing we simple need to ignore the target and not connect other values then target
+        Input: previous -> target(don't append) -> next
+        Output: previous -> next
+
+        Time Compleixty: O(n) - since iteartion stops when fast reached the end and slow connect to values
+        together so it's constant time
+
+        Space complexity O(1)
+        """
+
+        # if we have only one node
+        if head.next == None:
+            return None
+
+        # slow (n), fast (n+2)
+        slow, fast = head, head.next.next
+
+        # itereate two pointers till fast is not none
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # once fast is none, connect previous -> target (not connect it) -> next
+        slow.next = slow.next.next
+
+        # return the list
+        return head
---
+++ Linked List/328. Odd Even Linked List.py
@@ -0,0 +1,43 @@
+#https://leetcode.com/problems/odd-even-linked-list/?envType=study-plan-v2&envId=leetcode-75
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        """
+        To solve this problem we need to create two heads, one for even and one for odd
+
+        The odd head should start from first element
+        While even head should start from second element
+
+        Once odd will be fully traversed we would need to connect odd to even head.
+
+        Time Compleixty: O(n) - n total number of ndoes
+        Space Complexity; O(1)
+        """
+
+        # edge case if we have one value in a linked list
+
+        if head == None:
+            return None
+
+        # Odd (n), Even (n + 1)
+        odd, even = head, head.next
+
+        even_head = even
+
+        while even and even.next:
+            # point odd to the next odd node
+            odd.next = even.next
+            odd = odd.next
+            # point even to the next even node
+            even.next = odd.next
+            even = even.next
+
+        # connect odd with even head to make full sequence
+        odd.next = even_head
+        return  head
