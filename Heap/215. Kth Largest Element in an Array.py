b10ee056f230dc05381b7bc30bb0023bb3954663
omad <omadbek95@gmail.com>
2025-05-11 16:58:09 +0200

    fixed

--- Heap/215. Kth Largest Element in an Array.py
+++ Heap/215. Kth Largest Element in an Array.py
@@ -1,23 +1,37 @@
-#https://leetcode.com/problems/kth-largest-element-in-an-array/?envType=study-plan-v2&envId=leetcode-75
+#https://leetcode.com/problems/kth-largest-element-in-an-array/
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
         """
-        We need to find kth largest element from the heap
+        The problem requires us to find Kth largest element in the array.
 
-        Approach:
-        For that we can iterate through the heap and create a min-heap, we then will store only k elements
-        and thus we could simply took first element from the heap and it will be our result
+        What we can do is actually add up element to the array,
+        then we would check with the largest element so far in our heap,
+        if we found largest element then simply pop from heap and append with
+        new largest element
+
+        While array is empty in the beginning we just want to populate it.
+
+        Time Complexity: O(n) - since we are traversing through the whole array
         """
-        #k = 4
-        heap = []
+
+        maxHeap = []
 
         for i in range(len(nums)):
-            heapq.heappush(heap, nums[i])
-            if len(heap) > k:
-                heapq.heappop(heap)
+            # check if the curr length of heap is not more than K
+            if len(maxHeap) >= k:
+                # check root val with curr number
+                if nums[i] > maxHeap[0]:
+                    # remove root value
+                    heapq.heappop(maxHeap)
+                    # append with new larger value
+                    heapq.heappush(maxHeap, nums[i])
+                else:
+                    continue
+            else:
+                heapq.heappush(maxHeap, nums[i])
 
-        print(heap)
+        # return largest value (root value)
+        return maxHeap[0]
 
-        return heap[0]
