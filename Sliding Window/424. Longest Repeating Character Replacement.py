6039c4ec6b66e9947bce45131c8902683bd2a1ec
omad <omadbek95@gmail.com>
2025-06-02 14:00:33 +0500

    sliding window problem

---
+++ Sliding Window/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+
+        """
+        Approach:
+        The task is to find lognest substring that is possible by changing K charcaters on it. For that we can use sliding widnow approach.
+        We need to use hashset and append values to count how much A or B we have, once we calculated we need to keep our values in the window.
+        If they are in a window we need to maximize and retunr result of consequence characters
+        """
+        counter = {}
+
+        left = 0
+        result = 0
+
+        for right in range(len(s)):
+            counter[s[right]] = 1 + counter.get(s[right], 0)
+
+            while (right - left + 1) - max(counter.values()) > k:
+                counter[s[left]] -= 1
+                left += 1
+
+            result = max(result, right - left + 1)
+
+        return result
